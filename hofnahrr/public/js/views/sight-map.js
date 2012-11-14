b95a58f343bb241a381cf7f99ed56843ec1c0f2e
Sven <mail@svenadolph.net>
2012-11-14 21:47:03 +0100

    update map

--- hofnahrr/public/js/views/sight-map.js
+++ hofnahrr/public/js/views/sight-map.js
@@ -20,21 +20,22 @@ define([
 
         afterRender : function(){
             var map = null;
-            var hull = new L.LatLng(50.325, 11.940);
+            var data = this.model ? this.model.toJSON() : {};
+
+            var pos = new L.LatLng(data.location.latitude, data.location.longitude);
 
             map = new L.Map(this.$('#map')[0]);
             var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
             var cmUrl = 'http://{s}.tile.cloudmade.com/77aace98a9ec425f8f2cb228c484f71f/997/256/{z}/{x}/{y}.png';
             var osmAttrib='Map data © openstreetmap contributors';
             var osm = new L.TileLayer(cmUrl,{minZoom:8,maxZoom:18,attribution:osmAttrib});
-
-            map.setView(hull,18);
+            
+            map.setView(pos,14);
             map.addLayer(osm);
-
-            L.marker([50.325, 11.940]).addTo(map);
-        }
-
-        // TODO map implementation
+            L.marker(pos)
+                .addTo(map)
+                .bindPopup('<p>' + data.description + '</p>').openPopup();
+        },
     });
 
     return SightMapView;
