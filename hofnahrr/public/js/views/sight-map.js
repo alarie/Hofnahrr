3402989b6192bb21d3aba0cf36c48ffb9580f849
Sven <mail@svenadolph.net>
2012-11-14 18:37:42 +0100

    added map

--- hofnahrr/public/js/views/sight-map.js
+++ hofnahrr/public/js/views/sight-map.js
@@ -1,11 +1,13 @@
 /*global define*/
 define([
-    'underscore', 'backbone', 
+    'underscore', 'backbone',
     'views/templated-bridge',
+    'libs/leaflet-src',
 
     'text!tmpl/sight-map.tmpl',
 ], function (
-    _, Backbone, TemplatedBridgeView, 
+    _, Backbone, TemplatedBridgeView,
+    leaflet,
     tmplSightMap
 ) {
     'use strict';
@@ -15,6 +17,22 @@ define([
     SightMapView = TemplatedBridgeView.extend({
         tagname : 'div',
         template : tmplSightMap,
+
+        afterRender : function(){
+            var map = null;
+            var hull = new L.LatLng(50.325, 11.940);
+
+            map = new L.Map(this.$('#map')[0]);
+            var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            var cmUrl = 'http://{s}.tile.cloudmade.com/77aace98a9ec425f8f2cb228c484f71f/997/256/{z}/{x}/{y}.png';
+            var osmAttrib='Map data © openstreetmap contributors';
+            var osm = new L.TileLayer(cmUrl,{minZoom:8,maxZoom:18,attribution:osmAttrib});
+
+            map.setView(hull,18);
+            map.addLayer(osm);
+
+            L.marker([50.325, 11.940]).addTo(map);
+        }
 
         // TODO map implementation
     });
