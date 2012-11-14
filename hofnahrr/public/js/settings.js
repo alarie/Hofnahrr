33565eda3f24723a5442626c5cbf3ad09d2e25ea
Simon Schmidt <simon-schmidt@alarie.de>
2012-11-14 19:42:58 +0100

    settings

---
+++ hofnahrr/public/js/settings.js
@@ -0,0 +1,13 @@
+/*global define*/
+define([
+    
+], function () {
+    'use strict';
+
+    return {
+        BASE_URL : 'http://localhost:2024/',
+        API : {
+            PICTURE : 'picture'
+        }
+    };
+});
