6f172ee59dfc67447c4e1b1b6bff926ca4e38ce1
svendroid <mail@svenadolph.net>
2012-11-26 12:33:48 +0100

    sight map bubble first steps

--- hofnahrr/public/js/views/sight-map.js
+++ hofnahrr/public/js/views/sight-map.js
@@ -5,20 +5,31 @@ define([
     'libs/leaflet-src',
 
     'text!tmpl/sight-map.tmpl',
+    'text!tmpl/sight-map-bubble.tmpl'
 ], function (
     _, Backbone, TemplatedBridgeView,
     leaflet,
-    tmplSightMap
+    tmplSightMap,
+    tmplSightMapBubble
 ) {
     'use strict';
 
     var SightMapView, SightMapBubbleView;
 
-    SightMapBubbleView = TemplatedBridgeView.extend({});
+    SightMapBubbleView = TemplatedBridgeView.extend({
+        tagname : 'div',
+        template : tmplSightMapBubble
+    });
+
 
     SightMapView = TemplatedBridgeView.extend({
         tagname : 'div',
         template : tmplSightMap,
+
+        initialize : function () {
+            TemplatedBridgeView.prototype.initialize.apply(this, arguments);
+            this.bubble = new SightMapBubbleView();
+        },
 
         setModel : function (model) {
             this.model = model;
@@ -30,7 +41,7 @@ define([
                 this.render();
             }
 
-            //this.bubble.setModel(model);
+            this.bubble.setModel(model);
             //this.renderBubbleAtLocation(pos);
             this.panMapTo(pos);
         },
@@ -72,26 +83,34 @@ define([
 
         afterRender : function () {
             this.map = null;
-            var data = this.model ? this.model.toJSON() : {};
 
-            var pos = new L.LatLng(data.location.latitude, data.location.longitude);
+            var data = this.model ? this.model.toJSON() : {},
+                pos = new L.LatLng(data.location.latitude, data.location.longitude),
+                osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                cmUrl = 'http://{s}.tile.cloudmade.com/77aace98a9ec425f8f2cb228c484f71f/997/256/{z}/{x}/{y}.png',
+                osmAttrib = 'Map data © openstreetmap contributors',
+                osm = new L.TileLayer(cmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
 
-            var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            var cmUrl = 'http://{s}.tile.cloudmade.com/77aace98a9ec425f8f2cb228c484f71f/997/256/{z}/{x}/{y}.png';
-            var osmAttrib='Map data © openstreetmap contributors';
-            var osm = new L.TileLayer(cmUrl,{minZoom:8,maxZoom:18,attribution:osmAttrib});
-            
             this.map = new L.Map(this.$('#map')[0], {
                 center: pos,
                 zoom: 14,
                 layers: [osm]
             });
 
-            L.marker(pos)
-                .addTo(this.map)
-                .bindPopup('<p>' + data.description + '</p>').openPopup();
+            this.marker = L.marker(pos);
+            this.marker.addTo(this.map);
 
-            //fix for bug at first load of map / popup or marke is still on wrong position
+            // console.log(this.bubble.render().el);
+
+
+            // this.marker.bindPopup(this.bubble.render().el);
+            this.marker.openPopup();
+                //.bindPopup('<p>' + data.description + '</p>').openPopup();
+                // .bindPopup(this.bubble.render()).openPopup();
+
+
+
+            //fix for bug at first load of map / popup or marker is still on wrong position
             //http://stackoverflow.com/questions/10762984/leaflet-map-not-displayed-properly-inside-tabbed-panel
             L.Util.requestAnimFrame(this.map.invalidateSize, this.map, false, this.map._container);
 
