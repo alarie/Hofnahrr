f230dd02cf664ec2a37df11eb2def31f0d92320a
svendroid <mail@svenadolph.net>
2012-12-03 11:27:50 +0100

    gallery with lightbox

--- hofnahrr/public/js/views/sight-gallery.js
+++ hofnahrr/public/js/views/sight-gallery.js
@@ -3,12 +3,12 @@ define([
     'jam/bootstrap-sass/js/bootstrap-carousel',
     'underscore', 'backbone', 
     'views/templated-bridge',
-    'libs/kort',
+    'libs/jquery.lightbox-0.5',
 
     'text!tmpl/sight-gallery.tmpl',
 ], function (
     $, _, Backbone, TemplatedBridgeView,
-    kort, 
+    lightbox, 
     tmplSightGallery
 ) {
     'use strict';
@@ -21,17 +21,19 @@ define([
         template : tmplSightGallery,
 
         afterRender : function () {
-            var carousel = this.$('.carousel');
-            carousel.carousel();
-
-            this.$('.left').click(carousel, function () {
-                carousel.carousel('prev');
+            
+            $('a.lightbox').lightBox({
+                // overlayBgColor: '#FFF',
+                // overlayOpacity: 0.6,
+                imageLoading: './img/lightbox-ico-loading.gif',
+                imageBtnClose: './img/lightbox-btn-close.gif',
+                imageBtnPrev: './img/lightbox-btn-prev.gif',
+                imageBtnNext: './img/lightbox-btn-next.gif',
+                imageBlank: './img/lightbox-blank.gif',
+                containerResizeSpeed: 350,
+                txtImage: 'Bild', //TODO use translation
+                txtOf: 'von' //TODO user translation
             });
-            this.$('.right').click(carousel, function () {
-                carousel.carousel('next');
-            });
-
-            Kort.bind();
         }
     });
 
