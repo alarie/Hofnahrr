03697c9eb877d69f4c8501d248d2c377847ef334
Simon Schmidt <simon-schmidt@alarie.de>
2012-12-12 14:41:20 +0100

    Capistrano

--- hofnahrr/config/deploy/production.rb
+++ hofnahrr/config/deploy/production.rb
@@ -3,6 +3,8 @@ server "192.168.90.45", :app, :web, :db, :primary => true
 set :user, "student"
 set :deploy_to, "/home/student/hofnahrr/production"
 
+set :use_sudo, true
+
 set(:settings_file) { "#{deploy_to}/current/hofnahrr/public/js/settings.js" }
 set(:base_url) { "http://192.168.90.45:8080/" }
 
