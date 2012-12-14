1712bd2ede1f5b7347ab05e8ed9cfb931f0bd7b9
Simon Schmidt <simon-schmidt@alarie.de>
2012-12-14 11:06:35 +0100

    Capistrano

--- hofnahrr/resources/sights/put.js
+++ hofnahrr/resources/sights/put.js
@@ -1,9 +1,9 @@
-if (!this.unknown && !me) {
-    cancel("You are not authorized to do that", 401);
-}
-else {
+if (this.unknown || me) {
     if (typeof this.unknown === 'undefined') {
         this.unknown = false;
     }
     this.speakingId = this.name.toLowerCase().replace(/[\s!\?\$\.:]/g, '-');
+}
+else {
+    cancel("You are not authorized to do that", 401);
 }
