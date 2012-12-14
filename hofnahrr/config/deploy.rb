a9ccd7b3701694e3562802886171829971ed0009
Simon Schmidt <simon-schmidt@alarie.de>
2012-12-14 11:32:00 +0100

    capistrano

--- hofnahrr/config/deploy.rb
+++ hofnahrr/config/deploy.rb
@@ -15,3 +15,18 @@ set :deploy_via, :remote_cache
 # capistrano-ext options
 set :stages, ["dev", "production"]
 set :default_stage, "dev"
+
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+#
+namespace :deploy do
+    task :restart, :roles => :web do
+        settings = capture("cat #{settings_file}")
+
+        settings.gsub!(/BASE_URL\s*:\s*'([^']+)'/m, "BASE_URL : '#{base_url}'")
+
+        put(settings, settings_file)
+    end
+end
+
